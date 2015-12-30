a216982a0a294ecc28d681b086678da1af8e05c0
Yifan Sun <sunyifan112358@gmail.com>
2015-12-30 18:01:32 -0500

    Fix compilation error

--- src/test/com/logistics/pvis/example/ProcessingFrameTest.java
+++ src/test/com/logistics/pvis/example/ProcessingFrameTest.java
@@ -4,6 +4,8 @@
 package com.logistics.pvis.example;
 
 import com.logistics.pvis.application.Application;
+import com.logistics.pvis.frame.DelegatedPAppletFactory;
+import com.logistics.pvis.frame.JFrameFactory;
 import com.logistics.pvis.frame.ProcessingFrame;
 import com.logistics.pvis.frame.ProcessingFrameFactory;
 
@@ -17,7 +19,8 @@ public class ProcessingFrameTest {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		ProcessingFrameFactory frameFactory = new ProcessingFrameFactory();		
+		ProcessingFrameFactory frameFactory = new ProcessingFrameFactory(
+				new JFrameFactory(), new DelegatedPAppletFactory());		
 		ProcessingFrame frame =  frameFactory.produceProcessingFrame(960, 480);
 		
 		Application app = new App(frame);
