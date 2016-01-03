526de89223fc21c72b1472f934ccd1771f62aecd
Yifan Sun <sunyifan112358@gmail.com>
2016-01-03 01:56:07 -0500

    Update color equals function

--- src/main/com/logistics/pvis/color/Color.java
+++ src/main/com/logistics/pvis/color/Color.java
@@ -27,10 +27,40 @@ public class Color {
 	public static final Color WHITE = new Color(255);
 	public static final Color BLACK = new Color(0);
 	
-	public boolean equals(Color color) {
-		if (r == color.r && g == color.g && b == color.b && a == color.a) {
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + a;
+		result = prime * result + b;
+		result = prime * result + g;
+		result = prime * result + r;
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
 			return true;
-		}
-		return false;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Color other = (Color) obj;
+		if (a != other.a)
+			return false;
+		if (b != other.b)
+			return false;
+		if (g != other.g)
+			return false;
+		if (r != other.r)
+			return false;
+		return true;
 	}
 }
